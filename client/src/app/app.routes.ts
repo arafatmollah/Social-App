9d6cde64989c3d94793681c8fe4d795d79e14447
arafatmollah <arafatshuvo18@gmail.com>
2025-08-30 22:42:01 +0600

    add member components with routing and basic templates

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { MemberList } from '../features/members/member-list/member-list';
+import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
+import { Message } from '../features/members/message/message';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', Component: HomeComponent},
+    {path:'/member-list', component: MemberList},
+    {path:'/member-detail/:id', component: MemberDetailed},
+    {path:'/message', component:Message}
+];
